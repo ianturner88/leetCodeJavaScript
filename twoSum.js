af1a779aa8a6fd72e5bed61a921b3ee822224456
Ian Turner <ianturner@Ians-MacBook-Pro.local>
2020-03-02 21:36:20 -0500

    complete

--- twoSum.js
+++ twoSum.js
@@ -10,3 +10,37 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+
+var twoSum = function(nums, target) {
+    /* returns the indices of the two numbers that
+    add up to the target number*/
+
+    let match = false;
+    let sum = 0;
+    let indice1 = 0;
+    let indice2 = 0;
+
+    for (let anchor = 0; (anchor < nums.length) && (match === false); anchor++) {
+        /*sets the number to be added with all other numbers to the right of the anchor
+        number in the array*/
+
+        for (let i = (anchor + 1); (i < nums.length) && (match === false); i++) {
+            //add the numbers to the right of the anchor with the anchor to test for a match
+            sum = nums[anchor] + nums[i];
+
+            if (sum === target) {
+                //the target sum is found
+                match = true;
+                indice1 = anchor;
+                indice2 = i;
+                return [anchor, i];
+            }
+        }
+    }
+
+    return "No match!";
+};
+
+console.log(twoSum([1,2,9,5], 11));
+console.log(twoSum([13,17,21,1], 22));
+console.log(twoSum([10,20,90,50], 60));
